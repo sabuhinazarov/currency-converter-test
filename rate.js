ef0ed756a825a33148acad9ee9529fbca7b80172
Sabuhi Nazarov <sabuhinazarov@gmail.com>
2021-01-27 21:05:40 +0400

    cleaning

--- rate.js
+++ rate.js
@@ -9,7 +9,6 @@ const conversion_rates = [
 
 const conversion_map = new Object();
 
-
 conversion_rates.forEach(({ from, to, rate }) => {
     if (!conversion_map[from]) conversion_map[from] = new Object();
     conversion_map[from][to] = rate;
@@ -17,38 +16,29 @@ conversion_rates.forEach(({ from, to, rate }) => {
 
 
 function convert(from, to, amount) {
-    console.log("WHAT IS OUR TO", to)
-    const fromObj = conversion_map[from];
-    if (fromObj.hasOwnProperty(to)) return fromObj[to] * amount;
+    const firstObj = conversion_map[from];
+    if (firstObj && firstObj.hasOwnProperty(to)) return firstObj[to] * amount;
 
-    const finalObj = findRelation(fromObj, to);
-
-    console.log("secondObj qaaardasim", finalObj);
-
-    if (finalObj) {
-        const result = amount * finalObj;
-        return result;
-    }
+    const finalObj = findRelation(firstObj, to);
+    if (finalObj) return amount * finalObj;
     return "No rate found";
 }
 
 
 function findRelation(obj, to, value = 1) {
-    // const result = Object.keys(fromObj).find(i => conversion_map[i] && conversion_map[i].hasOwnProperty(to));
     let localValue = value;
     for(let key in obj) {
-        console.log("KEY", key, "TO", to);
-        if(key === to) {
-            return obj[key] * localValue
-        }
+        if(key === to)  return obj[key] * localValue;
+
         for(let subKey in conversion_map[key]) {
-            console.log("SUBKEY", subKey, "TO", to);
+
             if(subKey === to) {
                 return conversion_map[key][subKey] * localValue *  obj[key];
             } else localValue *=  conversion_map[key][subKey] *  obj[key];
+            
             return findRelation(conversion_map[subKey], to, localValue);
         }
     }
 }
 
-console.log(convert("USD", "TL", 1));
+console.log(convert("XXX", "AZN", 1));
